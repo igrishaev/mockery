92132290404ed9a7e9aff9bdc162b3fc1a5332f2
Ivan Grishaev <ivan@grishaev.me>
2017-03-27 23:31:48 +0300

    initial tests added

--- test/mockery/core_test.clj
+++ test/mockery/core_test.clj
@@ -1,7 +1,20 @@
 (ns mockery.core-test
   (:require [clojure.test :refer :all]
-            [mockery.core :refer :all]))
+            [mockery.core :refer [with-mock]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn test-fn [a b]
+  (+ a b))
+
+(deftest test-simple
+  (with-mock mock
+    {:target :test-fn
+     :return 42}
+    (let [result (test-fn 1 2)]
+      (is (= result 42))
+      (is (= @mock
+             {:called? true
+              :call-count 1
+              :call-args '(1 2)
+              :call-args-list '((1 2))
+              :target :test-fn
+              :return 42})))))
