4946a2f4d859b188d32ca8a2f7dc2438616d0167
Ivan Grishaev <ivan@grishaev.me>
2017-04-04 18:55:38 +0300

    docs updated

---
+++ index.clj
@@ -0,0 +1,21 @@
+{:namespaces
+ ({:doc "A library to mock Clojure functions.",
+   :name "mockery.core",
+   :wiki-url "https://igrishaev.github.io/mockeryindex.html",
+   :source-url
+   "https://github.com/igrishaev/mockery/blob/3cebe09c7f90dbf5bb6c70b013fd36ef54c9846c/src/mockery/core.clj"}),
+ :vars
+ ({:raw-source-url
+   "https://github.com/igrishaev/mockery/raw/3cebe09c7f90dbf5bb6c70b013fd36ef54c9846c/src/mockery/core.clj",
+   :name "with-mocks",
+   :file "src/mockery/core.clj",
+   :source-url
+   "https://github.com/igrishaev/mockery/blob/3cebe09c7f90dbf5bb6c70b013fd36ef54c9846c/src/mockery/core.clj#L82",
+   :line 82,
+   :var-type "macro",
+   :arglists ([bind-opt & body]),
+   :doc
+   "Like `with-mock` but for multiple mocks at once. `bind-opt` is a\nvector where each odd item is a bind symbol and even item is an\noption map. See `with-mock` for detailed description of each\nparameter.",
+   :namespace "mockery.core",
+   :wiki-url
+   "https://igrishaev.github.io/mockery/index.html#mockery.core/with-mocks"})}
